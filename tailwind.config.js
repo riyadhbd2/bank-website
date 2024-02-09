2babc42699f06bd987c64de8489b849db16633d7
Easir Arafat <riyadhbd2@Easirs-MacBook-Pro.local>
2024-02-09 13:29:25 +0100

    nav bar added

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-darl": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blob": "#A427DF",
+
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DAFAULT: '20px',
+        md: "50px"
+      }
+    }
+  },
+  plugins: [],
+}
+
