6bf804ef3c69d19179207a1ee986de7347dde089
Easir Arafat <riyadhbd2@Easirs-MacBook-Pro.local>
2024-02-09 14:22:37 +0100

    nav bar responsive added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         "color-primary": "#01051e",
         "color-primary-light": "#020726",
-        "color-primary-darl": "#010417",
+        "color-primary-dark": "#010417",
         "color-secondary": "#ff7d3b",
         "color-gray": "#333",
         "color-white": "#fff",
